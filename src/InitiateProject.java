2e6bd3d1774591543afb8b968a6add87206b92ba
Sebastian <se.hegardt@gmail.com>
2011-12-01 08:08:58 +0100

    Created third class to handle when a new game is to be created

--- src/InitiateProject.java
+++ src/InitiateProject.java
@@ -1,7 +1,7 @@
 public class InitiateProject{
 	public static void main (String [] args){
 		 //Produce the menu text, with options to start a NEW game or LOAD and old one
-		Menu.Start();
+		Menu.initiateMenu();
 	
 	}
 	
